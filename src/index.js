fff5f2b91dcccd9122b92395bbb5e2386b5ecc6c
teraf <teraf99@gmail.com>
2024-04-25 20:19:07 +0200

    add logos

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
-// ServiceWorkerRegistration.register();
 const root = createRoot(document.getElementById("root"));
 root.render(
     <App />
